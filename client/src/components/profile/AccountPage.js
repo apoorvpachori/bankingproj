aaaa1dcd89dc075efc20fb7f87debd9a15069fc1
Apoorv Pachori <apoorv.pachori21@gmail.com>
2021-05-03 18:00:17 -0700

    assignment

--- client/src/components/profile/AccountPage.js
+++ client/src/components/profile/AccountPage.js
@@ -1,6 +1,9 @@
 function AccountPage() {
   return (
     <div className="App">
+      {/* purpose of this page is to open / close accounts 
+    Write section of api that will create account for a user
+    create section of api that will close account for a user */}
       <h1>Account Page</h1>
       <h2>I am in the account page</h2>
     </div>
--- client/src/components/profile/TransferPage.js
+++ client/src/components/profile/TransferPage.js
@@ -1,6 +1,7 @@
 function TransferPage() {
   return (
     <div className="App">
+      {/* purpose of this page is to transfer funds from WITHIN a user */}
       <h1>Transfer Page</h1>
       <h2>I am in the Transfer page</h2>
     </div>
