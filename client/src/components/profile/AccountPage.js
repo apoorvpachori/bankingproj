6f3d78708a87a6210cdc92963745d37b10c61a6b
vthanhnguyen <vthanhnguyen95@yahoo.com>
2021-05-05 18:55:00 -0700

    changes to accountPage

--- client/src/components/profile/AccountPage.js
+++ client/src/components/profile/AccountPage.js
@@ -1,10 +1,12 @@
 import React from "react"
+import axios from "axios";
 class AccountPage extends React.Component {
     constructor(){
       super()
       this.state = {
-          amount1: "",
-          istrue: false,
+          acc1available: false,
+          acc2available: false,
+          acc3availble: false,
       }
       this.handleChange = this.handleChange.bind(this)
     }
@@ -16,6 +18,26 @@ class AccountPage extends React.Component {
     
       })
     }
+    componentDidMount(){
+
+    }
+
+    updateAcc = async () => {
+      await axios
+        .get(`http://localhost:7000/users/${this.props.user.id}`)
+        .then((res) => {
+          if (res.data[0].amount != null) {
+            this.setState({ acc1available:true });
+          }
+          else if(res.data[0].amount2 != null){
+            this.setState({ acc2available:true });
+          }
+          else if(res.data[0].amount2 != null){
+            this.setState({ acc3available:true });
+          }
+        });
+    };
+
     render(){
     return (
       <div className="App">
@@ -23,30 +45,32 @@ class AccountPage extends React.Component {
       Write section of api that will create account for a user
       create section of api that will close account for a user */}
         <h1>Open/Close Account Page</h1>
-        <h3>If you want to open a new Account, enter the account name: </h3>
-        <form>
-          <input type="text " />
-        </form>
+        {/* check if there are any avilable accounts to open
+          if theres any available accounts display them... */}
+        <h2>These are the availavble acounts to open: </h2>
+
         <br/><br/>
         <h3>If you want to delete an account, check the account and press submit:</h3>
         <form>
-        <input 
-          type="checkbox"
-          name ="amount1"
-          checked = {this.state.istrue}
-          onChange = {this.handleChange}
-        />  Account 1
+          
+          <input 
+            type="checkbox"
+            name ="amount1"
+            checked = {this.state.istrue}
+            onChange = {this.handleChange}
+          />  Account 1
+ 
         <br/>
         <input 
           type="checkbox"
-          name = "account 2"
+          name = "amount2"
           checked = {this.state.istrue}
           onChange = {this.handleChange}
         />  Account 2
         <br/>
         <input 
           type="checkbox"
-          name = "account 3"
+          name = "amount3"
           checked = {this.state.istrue}
           onChange = {this.handleChange}
         />  Account 3
