e3be0edb1aede4c2c93975a372ceb9d27f1c81c9
chafik304 <82192928+chafik304@users.noreply.github.com>
2021-05-12 20:36:23 -0700

    home text update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,9 +9,18 @@ class Home extends React.Component {
         <h1>Welcome to the Bank of React !</h1>
         <img src={logo} height='250' width = '200' />
         <br/><br/>
-        <p>This Banking Project was created by 6 students</p>
-        <p>This project was created by coding in JavaScript and the database is MySQL </p>
-        <p>add more info about the project...</p>
+        <p>This project was created using React.js and Node.js and the MySQL as a database. </p>
+        <p>This BankingProject is an Online ATM</p> 
+        <p>It is used to create an online bank account, to deposit a check or a money order, and to transfer funds from one account to the other </p>
+        <br/><br/>
+        <h5>About Creators</h5>  
+        <p>This Banking Project was created by 6 students at the univeristy of SJSU</p> 
+        <p>Apoorv Pachori: Software Developer</p> 
+        <p>Thanh Nguyen: Software Engineer</p> 
+        <p>Chafik Aziz: Systems Engineer</p>
+        <p>Stephen Oneto: Sofwtare Engineer</p> 
+        <p> Kelly Nguyen: Software Engineer</p>
+        <p> Leonardo Blas: Software Engineer </p>
       </div>
     );
   }
